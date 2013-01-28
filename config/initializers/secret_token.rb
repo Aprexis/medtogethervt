55be022ed7fefe67b0446a45fca10771ed22e6eb
Alan Lattimore <alan.lattimore@aprexis.com>
2013-01-28 16:12:39 -0500

    Initial check in.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Medtogethervt::Application.config.secret_token = '7071e3f00f79ecd1be1bd8a2042b6c6f57a4d11dd12ad8107e4c6e49bb8c9e68aae11a3b4f0c7c1de8e1447eafd052152fa58f53890e0a8fa45075ae3ac56394'
